6ce4c17a6d77706a8996b12d19f4af1301b0c660
Rich Jenks <rich@richjenks.com>
2017-07-25 23:42:54 +0100

    Added check for encoding

--- twilio-bulk.php
+++ twilio-bulk.php
@@ -15,18 +15,60 @@ $auth       = parse_ini_file(__DIR__ . '/config/auth.txt');
 $logger = new Katzgrau\KLogger\Logger(__DIR__.'/log');
 $client = new Twilio\Rest\Client($auth['account_id'], $auth['auth_token']);
 
-// Intro data
+// Message length limits
+$limit['single']   = 160;
+$limit['multiple'] = 153;
+$limit['ucs2']     = 70;
+
+// Valid chars
+$gsm7 = '@£$¥èéùìòÇLFØøCRÅåΔ_ΦΓΛΩΠΨΣΘΞESCÆæßÉSP!"#¤%&()*+,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà' . "'" . "\n" . "\r" . " " . "|^€{}[~]\\";
+$gsm7 = preg_split('//u', $gsm7, -1, PREG_SPLIT_NO_EMPTY);
+
+// Analyse chars
+$ucs2 = false;
+$chars = preg_split('//u', $message, -1, PREG_SPLIT_NO_EMPTY);
+fwrite(STDOUT, "\e[44m"); // Blue background
+fwrite(STDOUT, "\e[97m"); // White text
+foreach ($chars as $char) {
+	$valid = (in_array($char, $gsm7));
+	if (!$valid) fwrite(STDOUT, "\e[41m"); // Red background
+	fwrite(STDOUT, $char);
+	if (!$valid) fwrite(STDOUT, "\e[44m"); // Blue background
+	if (!$valid) $ucs2 = true;
+}
+fwrite(STDOUT, "\e[0m"); // Normal
+fwrite(STDOUT, "\n");
+
+// Analyse message
+$length = mb_strlen($message);
 $count = count($recipients);
-$messages = 1;
-$length = strlen($message);
-if ($length > 160) $messages = ceil($length / 153);
-$messages = $messages * $count;
+if ($ucs2) $split = $limit['ucs2'];
+elseif ($length > 160) $split = $limit['multiple'];
+else $split = $limit['single'];
+$messages = ceil($length / $split);
+$requests = $messages * $count;
+$encoding = ($ucs2) ? 'UCS-2' : 'GSM-7';
+
+// Message report
+fwrite(STDOUT, "Encoding:\t$encoding\n");
+fwrite(STDOUT, "Character(s):\t$length\n");
+fwrite(STDOUT, "Char Limit:\t$split\n");
+fwrite(STDOUT, "Recipients(s):\t$count\n");
+fwrite(STDOUT, "Messages:\t$messages\n");
+fwrite(STDOUT, "Requests:\t$requests\n");
+
+// Intro data
+// $count = count($recipients);
+// $messages = 1;
+// $length = strlen($message);
+// if ($length > 160) $messages = ceil($length / 153);
+// $messages = $messages * $count;
 
 // Welcome!
-fwrite(STDOUT, "\033[1;37m\033[44m");
-fwrite(STDOUT, "$message\n");
-fwrite(STDOUT, "\033[0m");
-fwrite(STDOUT, "$length character(s) | $count recipient(s) | $messages message(s)\n");
+// fwrite(STDOUT, "\033[1;37m\033[44m");
+// fwrite(STDOUT, "$message\n");
+// fwrite(STDOUT, "\033[0m");
+// fwrite(STDOUT, "$length character(s) | $count recipient(s) | $messages message(s)\n");
 
 // Are you sure?
 fwrite(STDOUT, "Type 'Twilio' to continue: ");
